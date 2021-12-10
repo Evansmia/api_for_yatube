4f5d03ad5328f15c3536391184ef2d545b278b4a
Mia Evans <miaevansla@gmail.com>
2021-12-10 09:34:39 +0300

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# api_final
-api final
+<h1>API для Yatube</h1>
+<h2>Краткое описание</h2>
+<li>Аутентификация по JWT-токену</li>
+<li>Работа со списком всех публикаций блога и отдельными постами по id</li>
+<li>Работа со списком всех комментариев и комментариев для публикации по id </li>
+<li>Работа с подписчиками</li>
+<li>Работа с группами</li>
+
+## Как запустить проект
+
+### Клонировать репозиторий
+```git clone https://github.com/Evansmia/api_final_yatube.git```
+### Создать и активировать виртуальное окружение
+```
+python3 -m venv venv<br>
+source venv/Scripts/activate  #для Windows
+source venv/bin/activate      #для Linux и macOS
+```
+### Установить зависимости из файла requirements.txt
+```
+pip install -r requirements.txt
+```
+### Выполнить миграции
+```
+python3 manage.py migrate
+```
+### Запустить проект
+```
+python manage.py runserver
+```
+
+### Приложение доступно по адресу: http://127.0.0.1:8000/
+### Документация API: http://127.0.0.1:8000/redoc/
--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,8 +1,10 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
+from rest_framework.validators import UniqueTogetherValidator
+from posts.models import Comment, Follow, Group, Post
 
-
-from posts.models import Comment, Post
+User = get_user_model()
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -21,3 +23,39 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         fields = '__all__'
         model = Comment
+
+
+class GroupSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Group
+        fields = '__all__'
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True,
+        default=serializers.CurrentUserDefault()
+    )
+    following = serializers.SlugRelatedField(
+        slug_field='username',
+        queryset=User.objects.all()
+    )
+
+    def validate_following(self, value):
+        if self.context.get('request', None).user == value:
+            raise serializers.ValidationError(
+                'Нельзя подписаться на самого себя.'
+            )
+        return value
+
+    class Meta:
+        fields = '__all__'
+        model = Follow
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Follow.objects.all(),
+                fields=['user', 'following']
+            )
+        ]
